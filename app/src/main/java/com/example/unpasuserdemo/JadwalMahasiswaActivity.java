fef851bae638dfc0cbd1ff2a71675d690ba375d1
Yadi Rudiyansah <zolandhole@gmail.com>
2019-04-03 21:05:39 +0700

    tambah

---
+++ app/src/main/java/com/example/unpasuserdemo/JadwalMahasiswaActivity.java
@@ -0,0 +1,14 @@
+package com.example.unpasuserdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JadwalMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal_mahasiswa);
+    }
+}
